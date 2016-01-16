7d1ae0005855d398dc1f6b2a22d67f2cbc56e141
Landon Bouma <landon.bouma@gmail.com>
2016-01-16 03:35:56 -0600

    Update setup.py.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name='bencodepy',
-    version='0.9.5',
-    packages=['bencodepy'],
-    url='https://github.com/eweast/bencodepy',
+    name='bhencode',
+    version='0.9.6',
+    packages=['bhencode'],
+    url='https://github.com/landonb/bhencode',
     license='GPLv2',
-    author='Eric Weast',
+    author='Eric Weast, Landon Bouma',
     author_email='eweast@hotmail.com',
     description='Bencode encoder/decoder written in Python 3 under the GPLv2.'
 )
